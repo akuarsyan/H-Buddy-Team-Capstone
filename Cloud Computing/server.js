48e9053e77525f2d9784ccff8248f18b31e7d57d
Lutfiarief <137787385+Lutfiarief@users.noreply.github.com>
2024-06-09 20:20:22 +0700

    Update server.js
    next

--- Cloud Computing/server.js
+++ Cloud Computing/server.js
@@ -3,8 +3,8 @@ const articleRoutes = require('./routes/articles');
 
 const init = async () => {
     const server = Hapi.server({
-        port: 3000,
-        host: 'localhost'
+        port: process.env.PORT,
+        host: '0.0.0.0',
     });
 
     server.route(articleRoutes);
