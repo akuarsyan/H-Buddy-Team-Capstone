823c58b757364b4567a4252d36dfa0db1fbad290
umarfornow <azizumar432@gmail.com>
2024-06-18 20:43:42 +0700

    Update server.js

--- Cloud Computing/article API/server.js
+++ Cloud Computing/article API/server.js
@@ -1,1 +1,21 @@
-contoh
+const Hapi = require('@hapi/hapi');
+const articleRoutes = require('./routes/articles');
+
+const init = async () => {
+    const server = Hapi.server({
+        port: process.env.PORT,
+        host: '0.0.0.0',
+    });
+
+    server.route(articleRoutes);
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+});
+
+init();
